9b2639d3fde6ca0c48137a11dd88031b30c3ccd1
Ismael Martínez <1077546@est.intec.edu.do>
2025-02-10 17:43:15 -0400

    27. Eventos - Respondiendo a Acciones del Usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit(): void {
-      throw new Error('Method not implemented.');
-    }
-
-    titulo = 'Juan';
+export class AppComponent {
 
     peliculasEnCines =
       [{
--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -14,5 +14,9 @@ export class ListadoPeliculasComponent implements OnInit{
 
   }
 
+  remover(indicePelicula: number): void {
+    this.peliculas.splice(indicePelicula, 1);
+    console.log(indicePelicula);
+  }
 
 }
